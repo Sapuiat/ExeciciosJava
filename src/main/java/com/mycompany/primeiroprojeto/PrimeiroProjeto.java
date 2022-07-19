da5c35e17eca149108bad2d101c8b536babd34ce
Miguel <miguelpay157@gmail.com>
2022-07-18 22:04:38 -0300

    first commit

---
+++ src/main/java/com/mycompany/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,11 @@
+package com.mycompany.primeiroprojeto;
+
+/**
+ *
+ * @author migue
+ */
+public class PrimeiroProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Brasil, contemplem o REI!");
+    }
