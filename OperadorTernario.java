da446f09b48d252076ce554c4c9b7d1282c7c85f
Miguel Nunes <108318528+Sapuiat@users.noreply.github.com>
2022-07-27 18:26:24 -0300

    Create OperadorTernario.java

---
+++ OperadorTernario.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.operadorternario;
+
+
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 8;
+        int r = (n1>n2)?n1:n2;
+        System.out.println(r);         
+    }
+}
