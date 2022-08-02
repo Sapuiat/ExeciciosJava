82f71b487656017f82976dd4883a6234f679a964
Miguel Nunes <108318528+Sapuiat@users.noreply.github.com>
2022-08-02 18:43:21 -0300

    PrimeiraExercPOO

---
+++ FuncaoPOO/FuncaoPOO.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.funcaopoo;
+
+
+public class FuncaoPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Vai começar a contagem");
+        System.out.println(Operacoes.contador(1,6));
+    }
+}
---
+++ FuncaoPOO/Operacoes.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.funcaopoo;
+
+
+public class Operacoes {
+    static String contador (int i, int f) {
+      String s = "";
+      for (int c = i; c<= f; c++) {
+          s += c + " ";
+        }
+    return s;
+    }  
+}
