59ae3ccb06e5dbcf574738043d5454406d6c1db1
Miguel Nunes <108318528+Sapuiat@users.noreply.github.com>
2022-08-02 18:46:36 -0300

    Create Vetor01.java

---
+++ Vetor01.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.vetor01;
+
+
+public class Vetor01 {
+
+    public static void main(String[] args) {
+        int n[] = {3,4,5,6,7};
+        System.out.println("O tamanho do vetor é de "+ n.length);
+        for(int c = 0; c<n.length; c++){
+            System.out.println("Na posição " + c +" Temos o valor " +n[c]);
+            
+        }
+    }
+}
