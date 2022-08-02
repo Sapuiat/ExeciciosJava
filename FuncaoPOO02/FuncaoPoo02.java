5d2ebe8cbff758147568011d781bcff9e0d191df
Miguel Nunes <108318528+Sapuiat@users.noreply.github.com>
2022-08-02 18:45:33 -0300

    FuncaoFormulaFat

---
+++ FuncaoPOO02/FuncaoPoo02.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.funcaopoo02;
+
+
+public class FuncaoPoo02 {
+
+    public static void main(String[] args) {
+        Fatorial f = new Fatorial();
+        f.setValor(5);
+        System.out.print(f.getFormula());
+        System.out.print(f.getFatorial());
+    }
+}
